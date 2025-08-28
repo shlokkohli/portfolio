5f334283a8256b5413e318ee766b6bccb73eb2fe
Shlok Kohli <shlokkohli11@gmail.com>
2025-08-29 01:02:56 +0530

    image url changed

--- src/app/helpers/MyProject.ts
+++ src/app/helpers/MyProject.ts
@@ -1,6 +1,6 @@
 export const MyProjects = [
     {
-        imageURL: "./WiseUp.png",
+        imageURL: "/WiseUp.png",
         title: "WiseUp",
         description: "Transaction management SaaS platform with Next.js, Stripe, and Vercel Cron Jobs.Includes visualizations, automated recurring payments, and Gemini AI-powered spending insights.",
         TechStack: ["Next.js", "Zod", "Clerk", "Stripe", "NeonDB"],
@@ -8,7 +8,7 @@ export const MyProjects = [
         WebsiteLink: "https://wise-up.vercel.app/"
     },
     {
-        imageURL: "./Anonymous_Message.png",
+        imageURL: "/Anonymous_Message.png",
         title: "Anonymous Messaging",
         description: "Secure platform built with Next.js, TypeScript, MongoDB, and Zod validation. Features Gemini-powered smart suggestions and a modern animated UI with shadcn/ui and Aceternity UI.",
         TechStack: ["Next.js", "Zod", "Gemini", "Aceternity UI"],
@@ -16,7 +16,7 @@ export const MyProjects = [
         WebsiteLink: "https://anonymous-message-gray.vercel.app/u/shlok"
     },
     {
-        imageURL: "./Chatterly.png",
+        imageURL: "/Chatterly.png",
         title: "Chatterly",
         description: "Real-time chat app using WebSockets and Zustand for low-latency messaging. Supports instant delivery, private chats, and a scalable architecture for smooth communication.",
         TechStack: ["MongoDB", "MERN", "WebSockets", "Zustand"],
@@ -24,7 +24,7 @@ export const MyProjects = [
         WebsiteLink: "https://chatapp-e7hr.onrender.com/"
     },
     {
-        imageURL: "./X_Clone.png",
+        imageURL: "/X_Clone.png",
         title: "Twitter",
         description: "MERN stack app with TanStack Query for efficient data fetching. Includes user auth, personalized feeds, likes, comments, follows, profiles, and real-time interactions and more.",
         TechStack: ["MongoDB", "MERN", "TanStack Query", "React"],
